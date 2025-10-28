26ebd8a90c4541eef4f831079cbcdfbe69f88115
janessamaeignaco-bit <ignacojanessamae@gmail.com>
2025-10-29 03:21:56 +0800

    Resolved conflict.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-print(“DCIT 26 – Midterm Practical Laboratory”)
-<<<<<<< HEAD
-print(“Submitted by Ignaco, Janessa Mae ”)
-=======
-print(“Submitted by Ignaco, Janessa Mae ”)
-print(“Date of Submission: October 28, 2025")
->>>>>>> janessa_mae
+print("DCIT 26 - Midterm Practical Laboratory")
+print("Submitted by Ignaco, Janessa Mae ")
+print("Date of Submission: October 28, 2025")
